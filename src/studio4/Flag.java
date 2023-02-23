1ea7309a74c8b7d53e8710838dce064711958eff
ayahabdelhameed <ayahabdelhameed@ayahs-air.dhcp.wustl.edu>
2023-02-23 12:28:30 -0600

    LuvIsland Flag by Ayah and Erin

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,30 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+	
+		StdDraw.clear();
+		
+		// background
+		Color pink = new Color(229, 188, 203);
+		
+		StdDraw.setPenColor(pink);
+
+		StdDraw.filledRectangle(.5, .5, .4, .25);
+
+		//envelope left side
+		double[] x = {.1,  .9, .1};
+		double[] y = {.25,  .25, .75};
+		Color darkPink = new Color(236, 156, 185);
+
+		
+		StdDraw.setPenColor(darkPink);
+
+		StdDraw.filledPolygon(x,y);
+		
+		//envelope right side
+		double[] a = {.9,  .9, .1};
+		double[] b = {.75,  .25, .25};
+		StdDraw.filledPolygon(a,b);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,19 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		Color pink = new Color (redComponent, greenComponent, blueComponent);
+		StdDraw.setPenColor(pink);
+
+		StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
 	}
 }
